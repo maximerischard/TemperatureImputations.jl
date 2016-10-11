1921a26e250a6d5fb407fece28ecdf312643b879
maximerischard <rischardmaxime@gmail.com>
2016-10-10 20:16:51 -0400

    perform imputations in STAN using predictive mean and covariance obtained analytically from GP using nearby hourly data

---
+++ src/variogram.jl
@@ -0,0 +1,26 @@
+function variogram(times, values, bins)
+    npairs = zeros(Int, length(bins))
+    sqdiff_accum = zeros(Float64, length(bins))
+    n = length(times)
+    for (i, t1) in enumerate(times)
+        ibin = 1
+        for j in i:n
+            Δt = times[j] - t1
+            while Δt>bins[ibin]
+                ibin += 1
+            end
+            npairs[ibin] += 1
+            sqdiff_accum[ibin] += (values[j]-values[i])^2
+        end
+    end
+    return sqdiff_accum ./ (npairs.*2)
+end
+
+function cov(sumkern::SumKernel, r::Float64)
+    ck = 0.0
+    for k in sumkern.kerns
+        metr = evaluate(metric(k), [r], [0.0])
+        ck += cov(k, metr)
+    end
+    return ck
+end
