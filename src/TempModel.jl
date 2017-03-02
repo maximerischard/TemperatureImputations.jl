eb9aa1120b79f16140e16f2c2c00cc114ff73e02
maximerischard <rischardmaxime@gmail.com>
2017-03-02 15:19:24 -0500

    updates to notebooks

--- src/TempModel.jl
+++ src/TempModel.jl
@@ -4,9 +4,11 @@ module TempModel
     using Optim
     using Mamba
     using Stan
+    import Proj4
     include("GPrealisations.jl")
     include("utils.jl")
     include("preprocessing.jl")
     include("predict_from_nearby.jl")
     include("stan_impute.jl")
+    include("fitted_kernel.jl")
 end
