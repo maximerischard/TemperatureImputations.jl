de55b77b439fba85c017324a96d00b5734cbce2b
maximerischard <rischardmaxime@gmail.com>
2017-08-31 15:12:00 +0100

    edit stan model for publication

--- src/stan_impute.jl
+++ src/stan_impute.jl
@@ -25,70 +25,72 @@ function prep_data(nearby_pred::NearbyPrediction, TnTx::DataFrame,
         "predicted_mean" => μ_window,
         "predicted_cov" => Σ_window.mat,
         "predicted_cov_chol" => full(Σ_window.chol[:L]),
-        "k_softmax" => 10.0,
+        "k_smoothmax" => 10.0,
     )
     return imputation_data, ts_window
 end
 
 function get_imputation_model()
     imputation_model = """
-    functions {
-        real softmax(vector x, real k){
-            real max10x;
-            max10x = max(k*x);
-            return (max10x+log(sum(exp(k*x - max10x))))/k;
-        }
-        real softmin(vector x, real k){
-            return -softmax(-x, k);
-        }
-    }
-    data {
-        // Tn Tx data
-        int<lower=1> N_TxTn; //
-        vector[N_TxTn] Tx;
-        vector[N_TxTn] Tn;
-        
-        // imputation points (for which we have )
-        int<lower=1> Nimpt;
-        int<lower=1,upper=N_TxTn> day_impute[Nimpt];
-        // number of hours recorded within each day
-        int<lower=1> impt_times_p_day[N_TxTn];
-        
-        // prior 
-        vector[Nimpt] predicted_mean;
-        matrix[Nimpt,Nimpt] predicted_cov;
-        matrix[Nimpt,Nimpt] predicted_cov_chol;
-        
-        // control soft max hardness
-        real<lower=0> k_softmax;
-    }
-    parameters {
-        vector[Nimpt] w_uncorr;
-        real mu;
-    }
-    transformed parameters {
-        vector[Nimpt] temp_impt;
-        real Tsoftmax[N_TxTn];
-        real Tsoftmin[N_TxTn];  
-        temp_impt = mu + predicted_mean + predicted_cov_chol*w_uncorr;
-        {
-            int istart;
-            istart = 1;
-            for (i in 1:N_TxTn){
-                int ntimes;
-                ntimes = impt_times_p_day[i];
-                Tsoftmin[i] = softmin(segment(temp_impt,istart,ntimes), k_softmax);
-                Tsoftmax[i] = softmax(segment(temp_impt,istart,ntimes), k_softmax);
-                istart = istart + ntimes;
+        functions {
+            real smoothmax(vector x, real k, real maxkx){
+                return (maxkx+log(sum(exp(k*x - maxkx))))/k;
+            }
+            real smoothmin(vector x, real k, real minkx){
+                return -smoothmax(-x, k, -minkx);
             }
         }
-    }
-    model {
-        w_uncorr ~ normal(0,1);
-        mu ~ normal(0, 100.0);
-        Tn ~ normal(Tsoftmin, 0.1);
-        Tx ~ normal(Tsoftmax, 0.1);
-    }
+        data {
+            // Tn Tx data
+            int<lower=1> N_TxTn; //
+            vector[N_TxTn] Tx;
+            vector[N_TxTn] Tn;
+
+            // imputation points (for which we have )
+            int<lower=1> Nimpt;
+            int<lower=1,upper=N_TxTn> day_impute[Nimpt];
+            // number of hours recorded within each day
+            int<lower=1> impt_times_p_day[N_TxTn];
+
+            // prior 
+            vector[Nimpt] predicted_mean;
+            matrix[Nimpt,Nimpt] predicted_cov;
+            matrix[Nimpt,Nimpt] predicted_cov_chol;
+
+            // control smooth max hardness
+            real<lower=0> k_smoothmax;
+        }
+        parameters {
+            vector[Nimpt] w_uncorr;
+            real mu;
+        }
+        transformed parameters {
+            vector[Nimpt] temp_impt;
+            real Tsmoothmax[N_TxTn];
+            real Tsmoothmin[N_TxTn];  
+            temp_impt = mu + predicted_mean + predicted_cov_chol*w_uncorr;
+            {
+                int istart;
+                istart = 1;
+                for (i in 1:N_TxTn){
+                    int ntimes;
+                    ntimes = impt_times_p_day[i];
+                    Tsmoothmin[i] = smoothmin(segment(temp_impt,istart,ntimes), 
+                                              k_smoothmax, 
+                                              k_smoothmax*Tn[i]);
+                    Tsmoothmax[i] = smoothmax(segment(temp_impt,istart,ntimes), 
+                                              k_smoothmax,
+                                              k_smoothmax*Tx[i]);
+                    istart = istart + ntimes;
+                }
+            }
+        }
+        model {
+            w_uncorr ~ normal(0,1);
+            mu ~ normal(0, 100.0);
+            Tn ~ normal(Tsmoothmin, 0.1);
+            Tx ~ normal(Tsmoothmax, 0.1);
+        }
     """
     stanmodel = Stanmodel(name="imputation", model=imputation_model)
     return stanmodel
