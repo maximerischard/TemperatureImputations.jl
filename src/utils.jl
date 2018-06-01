29862b29a8d565db4ff0f864cb4bf5be3f161378
maximerischard <rischardmaxime@gmail.com>
2018-06-01 17:19:05 +0100

    add distance on unit sphere function to source (not used in production)

--- src/utils.jl
+++ src/utils.jl
@@ -22,3 +22,38 @@ function measurement_date(t::DateTime, hr_measure::Hour)
         return Date(t)+Day(1)
     end
 end
+
+"""
+    Distance on units sphere, 
+    see (http://www.johndcook.com/blog/python_longitude_latitude/).
+"""
+function distance_on_unit_sphere(lat1, long1, lat2, long2)
+ 
+    # Convert latitude and longitude to 
+    # spherical coordinates in radians.
+    degrees_to_radians = π/180.0
+         
+    # phi = 90 - latitude
+    phi1 = (90.0 - lat1)*degrees_to_radians
+    phi2 = (90.0 - lat2)*degrees_to_radians
+         
+    # theta = longitude
+    theta1 = long1*degrees_to_radians
+    theta2 = long2*degrees_to_radians
+         
+    # Compute spherical distance from spherical coordinates.
+         
+    # For two locations in spherical coordinates 
+    # (1, theta, phi) and (1, theta', phi')
+    # cosine( arc length ) = 
+    #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+    # distance = rho * arc length
+     
+    cosangle = (sin(phi1)*sin(phi2)*cos(theta1 - theta2) +
+           cos(phi1)*cos(phi2))
+    arc = acos( cosangle )
+ 
+    # Remember to multiply arc by the radius of the earth 
+    # in your favorite set of units to get length.
+    return arc
+end
