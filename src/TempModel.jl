c10f40f71c71fcc4e4ac68d065950c5057bf2934
maximerischard <rischardmaxime@gmail.com>
2018-06-01 17:18:31 +0100

    add variogram to TempModel

--- src/TempModel.jl
+++ src/TempModel.jl
@@ -2,13 +2,14 @@ module TempModel
     using GaussianProcesses
     using PDMats
     using Optim
-    # using Mamba
     using Stan
     import Proj4
+    import NLopt
     include("GPrealisations.jl")
     include("utils.jl")
     include("preprocessing.jl")
     include("predict_from_nearby.jl")
     include("stan_impute.jl")
     include("fitted_kernel.jl")
+    include("variogram.jl")
 end
