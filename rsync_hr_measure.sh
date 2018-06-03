c8d852ca2a81a82cc3d11026d1f64e07b3d36c0c
maximerischard <rischardmaxime@gmail.com>
2018-06-03 23:06:57 +0100

    rsync doesn't go to background, add command line argument

--- rsync_hr_measure.sh
+++ rsync_hr_measure.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Define source, target, maxdepth and cd to source
-source="/Volumes/Samsung250GB/temperature_model/saved/hr_measure/SExSE"
-target="ody:/n/regal/pillai_lab/mrischard/temperature_model/saved/hr_measure/SExSE"
+GPmodel="$1"
+source="/Volumes/Samsung250GB/temperature_model/saved/hr_measure/$GPmodel"
+target="ody:/n/regal/pillai_lab/mrischard/temperature_model/saved/hr_measure/$GPmodel"
 depth=1
 cd "${source}"
 
 # Set the maximum number of concurrent rsync threads
 maxthreads=5
 # How long to wait before checking the number of rsync threads again
-sleeptime=5
+sleeptime=10
 
 # Find all folders in the source directory within the maxdepth level
 find . -maxdepth ${depth} -type d | while read dir
@@ -28,12 +29,18 @@ do
         #     chmod --reference="${source}/${subfolder}" "${target}/${subfolder}"
         # fi
         # Make sure the number of rsync threads running is below the threshold
-        while [ `ps -ef | grep -c [r]sync` -gt ${maxthreads} ]
+        while [ `ps -ef | grep -c "[r]sync --verbose"` -gt ${maxthreads} ]
         do
             echo "Sleeping ${sleeptime} seconds"
             sleep ${sleeptime}
         done
         # Run rsync in background for the current subfolder and move on to the next one
-        nohup rsync --verbose --human-readable --progress --archive --compress  "${target}/${subfolder}/" "${source}/${subfolder}/" </dev/null >/dev/null 2>&1 &
+        # nohup rsync --verbose --human-readable --progress --archive --compress  --update "${target}/${subfolder}/" "${source}/${subfolder}/" </dev/null >/dev/null 2>&1 &
+        rsync --verbose --human-readable --progress --archive --compress  --update "${target}/${subfolder}/" "${source}/${subfolder}/" &
     fi
+done
+while [ `ps -ef | grep -c "[r]sync --verbose" -gt 0 ]
+do
+    echo "Sleeping 30 seconds; waiting for all rsync jobs to finish"
+    sleep 30
 done
