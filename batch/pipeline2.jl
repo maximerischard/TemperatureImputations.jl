e657f0d60f30054b341a962c3c2aa837dc10a6e1
maximerischard <rischardmaxime@gmail.com>
2018-11-06 17:35:05 +0000

    start generalizing pipeline2 to other locations

--- batch/pipeline2.jl
+++ batch/pipeline2.jl
@@ -6,7 +6,7 @@ doc = """
     This script constrains those posteriors to be within the measured Tn&Tx.
 
     Usage:
-        pipeline2.jl <windownum> <model> <data_dir> <save_dir>
+        pipeline2.jl <ICAO> <windownum> <model> <data_dir> <save_dir>
 """
 using DocOpt
 arguments = docopt(doc)
@@ -17,52 +17,64 @@ println("directory for saved files: ", save_dir)
 data_dir = arguments["<data_dir>"]
 data_dir = joinpath(data_dir)
 windownum = parse(Int, arguments["<windownum>"])
+ICAO = arguments["<ICAO>"]
+@show ICAO
 GPmodel = arguments["<model>"]
+@show GPmodel
 
 using CmdStan
+using Dates
 using Dates: Day, Hour, Date, DateTime
 using JLD
+using CSV
 using TempModel
 using LinearAlgebra
 using PDMats
+using DataFrames
+using Printf
 
 stan_days = Day(9)
 stan_increment = Day(3)
 
-isdList = TempModel.read_isdList(; data_dir=data_dir)
-isdSubset=isdList[[(usaf in (725450,725460,725480,725485)) for usaf in isdList[:USAF]],:]
-isdSubset
-
-hourly_cat=TempModel.read_Stations(isdSubset; data_dir=data_dir)
-itest=3
-test_usaf=isdSubset[itest,:USAF]
+isdList = dropmissing(TempModel.read_isdList(; data_dir=data_dir))
+test_station = isdList[isdList[:ICAO].==ICAO, :]
+@assert nrow(test_station) == 1
+USAF = test_station[1, :USAF]
+WBAN = test_station[1, :WBAN]
+@show USAF
+@show WBAN
 hr_measure = Hour(17)
 
-TnTx = TempModel.test_data(hourly_cat, itest, hr_measure)
+let
+    hourly_test=TempModel.read_Stations(test_station; data_dir=data_dir)
+    itest=1
+    global TnTx = TempModel.test_data(hourly_test, itest, hr_measure)
+end
 
 struct FittingWindow
     start_date::Date
     end_date::Date
 end
 
-function predictions_fname(usaf::Int, fw::FittingWindow)
-    return @sprintf("%d_%s_to_%s.jld", 
-                    usaf, fw.start_date, fw.end_date)
+function predictions_fname(usaf::Int, wban::Int, icao::String, fw::FittingWindow)
+     @sprintf("%d_%d_%s_%s_to_%s.jld", 
+        usaf, wban, icao,
+        Date(fw.start_date), Date(fw.end_date))
 end
 
 # copy-pasted from pipeline1.jl
 nearby_windows = FittingWindow[]
-dt_start=DateTime(2015,1,1,0,0,0)
-increm=(maximum(hourly_cat[:ts])-minimum(hourly_cat[:ts])) / 15
+mintime = DateTime(2015,1,1,0,0,0)
+maxtime = DateTime(2016,1,1,0,0,0)
+increm=(maxtime-mintime) / 15
+dt_start = mintime
 window=3*increm
 while true
 	global dt_start
     dt_end=dt_start+window
-    sdate = Date(dt_start)
-    edate = Date(dt_end)
     fwindow = FittingWindow(sdate,edate)
     push!(nearby_windows, fwindow)
-    if dt_end >= (maximum(hourly_cat[:ts]))
+    if dt_end >= maxtime
         break
     end
     dt_start+=increm
@@ -105,36 +117,35 @@ println("STAN fitting window: ", stan_window)
 best_window = find_best_window(stan_window, nearby_windows)
 println("using nearby-predictions from: ", best_window)
 
-nearby_pred=load(joinpath(save_dir,
-                        "predictions_from_nearby",
-                        GPmodel,
-                        predictions_fname(test_usaf, best_window),
-                ))["nearby_pred"];
+nearby_path = joinpath(save_dir, "predictions_from_nearby", GPmodel,
+                       ICAO, predictions_fname(USAF, WBAN, ICAO, best_window))
+@show nearby_path
+nearby_pred=load(nearby_path)["nearby_pred"];
 
 imputation_data, ts_window = TempModel.prep_data(nearby_pred, TnTx, stan_window.start_date, hr_measure, stan_days)
 
-function stan_dirname(usaf::Int, fw::FittingWindow)
-    return @sprintf("%d_%s_to_%s/", 
-                    usaf, fw.start_date, fw.end_date)
+function stan_dirname(usaf::Int, wban::Int, icao::String, fw::FittingWindow)
+    return @sprintf("%d_%d_%s_%s_to_%s/", 
+                    usaf, wban, icao, fw.start_date, fw.end_date)
 end
 
-stan_dir = joinpath(save_dir,"stan_fit", GPmodel, stan_dirname(test_usaf, stan_window))
+stan_dir = joinpath(save_dir,"stan_fit", GPmodel, ICAO, stan_dirname(USAF, WBAN, ICAO, stan_window))
 if !isdir(stan_dir)
     mkpath(stan_dir)
 end
 imputation_model = TempModel.get_imputation_model(; pdir=stan_dir)
 for fname in readdir(joinpath(stan_dir, "tmp"))
-    mv(joinpath(stan_dir, "tmp", fname), joinpath(stan_dir, fname); remove_destination=true)
+    mv(joinpath(stan_dir, "tmp", fname), joinpath(stan_dir, fname); force=true)
 end
 rm(joinpath(stan_dir, "tmp"))
 
-writecsv(joinpath(stan_dir,"timestamps.csv"), reshape(ts_window, length(ts_window), 1))
+CSV.write(joinpath(stan_dir,"timestamps.csv"), reshape(ts_window, length(ts_window), 1))
 
 for fname in ("imputation","imputation_build.log","imputation_run.log","imputation.hpp")
     tmpdir = joinpath(save_dir, "..", "tmp")
     file_path = joinpath(tmpdir, fname)
     if isfile(file_path)
-        cp(file_path, joinpath(stan_dir,fname), remove_destination=true)
+        cp(file_path, joinpath(stan_dir,fname), force=true)
     else
         println(file_path, "NOT FOUND")
     end
