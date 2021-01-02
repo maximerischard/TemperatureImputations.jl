e667b940f853a5f122a85e6a58c41e0c5d746dcd
maximerischard <rischardmaxime@gmail.com>
2021-01-02 13:10:38 +0000

    clean up preprocessing.jl

--- src/preprocessing.jl
+++ src/preprocessing.jl
@@ -12,9 +12,9 @@ function read_station(usaf::Int, wban::Int, id::Int; data_dir::String=".")
                             )
     DataFrames.dropmissing!(station_data, disallowmissing=true)
     # remove missing data (null or nan)
-    station_data = station_data[.!ismissing.(station_data[!,:temp]), :]
+    station_data = station_data[.!ismissing.(station_data.temp), :]
     # station_data = station_data[.!isnan.(station_data[:temp]), :]
-    station_data = station_data[isfinite.(station_data[!,:temp]), :]
+    station_data = station_data[isfinite.(station_data.temp), :]
     # station_data[:temp] = Float64.(station_data[:temp])
     station_ts = DateTime[DateTime(r[:year], r[:month], r[:day],
                                    r[:hour], r[:min], r[:seconds]) 
@@ -34,7 +34,7 @@ function read_isdList(;data_dir::String=".", epsg::Int)
     # Project onto Euclidean plane
     epsg=Proj4.Projection(Proj4.epsg[epsg])
     wgs84=Proj4.Projection("+proj=longlat +datum=WGS84 +no_defs")
-    isdProj = Proj4.transform(wgs84, epsg, hcat(isdList[!,:LON], isdList[!,:LAT]))
+    isdProj = Proj4.transform(wgs84, epsg, hcat(isdList.LON, isdList.LAT))
     isdList[!,:X_PRJ] = isdProj[:,1]
     isdList[!,:Y_PRJ] = isdProj[:,2]
     return isdList
@@ -48,7 +48,7 @@ function stations_with_data(isdList::DataFrame; data_dir::String=".")
 end
 function add_ts_hours!(df::DataFrame)
     # timestamps in hours
-    ts_vals = df[!,:ts]
+    ts_vals = df.ts
     min_dt = minimum(ts_vals)
     ms_per_hour = convert(Millisecond, Hour(1))
     g_hours_from_min = (dt) -> convert(Millisecond, dt - min_dt) / ms_per_hour
@@ -64,25 +64,25 @@ function read_Stations(isdSubset; data_dir::String=".")
     return hourly_cat
 end
 function test_data(hourly::DataFrame, istation::Int, hr_measure::Hour)
-    hourly_test = hourly[hourly[!,:station] .== istation,:]
-    hourly_test[!,:ts_day] .= measurement_date.(hourly_test[!,:ts], hr_measure)
+    hourly_test = hourly[hourly.station .== istation,:]
+    hourly_test.ts_day .= measurement_date.(hourly_test.ts, hr_measure)
     TnTx = DataFrames.combine(
         DataFrames.groupby(hourly_test, :ts_day), 
         df -> DataFrame(
-            Tn=minimum(df[!,:temp]), 
-            Tx=maximum(df[!,:temp]),
-            Tn_time=df[argmin(df[!,:temp]),:ts],
-            Tx_time=df[argmax(df[!,:temp]),:ts],
+            Tn=minimum(df.temp), 
+            Tx=maximum(df.temp),
+            Tn_time=df[argmin(df.temp),:ts],
+            Tx_time=df[argmax(df.temp),:ts],
             times_p_day=DataFrames.nrow(df),
         )
     )
     return TnTx
 end
 function find_nearest(isdList::DataFrame, USAF::Int, WBAN::Int, k_nearest::Int)
-    test_station = isdList[(isdList[!,:USAF].==USAF) .& (isdList[!,:WBAN].==WBAN), :]
+    test_station = isdList[(isdList.USAF.==USAF) .& (isdList.WBAN.==WBAN), :]
     @assert nrow(test_station) == 1
-    distances = sqrt.((test_station[:X_PRJ].-isdList[:X_PRJ]).^2 
-                   .+ (test_station[:Y_PRJ].-isdList[:Y_PRJ]).^2)
+    distances = sqrt.((test_station.X_PRJ.-isdList.X_PRJ).^2 
+                   .+ (test_station.Y_PRJ.-isdList.Y_PRJ).^2)
     nearest_rows = sortperm(distances)
     nearest_and_test = isdList[nearest_rows[1:2+k_nearest-1], :]
     return nearest_and_test
