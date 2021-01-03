2af208cfdc95983987c8d9ef7e8f8d3bc4867c32
maximerischard <rischardmaxime@gmail.com>
2021-01-03 15:15:19 +0000

    continue building pipeline

--- batch/build_pipeline.sh
+++ batch/build_pipeline.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# exit when any command fails
+set -e
 
 ROOT=$(grealpath "$(dirname $0)/..")
 test_stations_path="${ROOT}/data/test_stations.csv"
@@ -13,7 +15,7 @@ for ICAO in ${test_stations_string}; do
     # https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop
     (i=i%nbatch) ; ((i++==0)) && wait
 
-    echo "ICAO: " $ICAO
+    echo "ICAO parameter fitting: " $ICAO
 # echo $test_stations | xargs -I {} \
     dvc run \
         --name kernelfit_${ICAO}_${GPmodel} \
@@ -31,9 +33,12 @@ done
 
 wait
 
+echo "--- predict from nearby ---"
 for ICAO in ${test_stations_string}; do
     # control the number of simultaneous processes
+    echo "ICAO predict from nearby: " $ICAO
     (i=i%nbatch) ; ((i++==0)) && wait
+    kerneljson="data/outputs/fitted_kernel/mll/${GPmodel}/hyperparams_${GPmodel}_${ICAO}.json"
     dvc run \
         --name nearby_predictions_${ICAO}_${GPmodel} \
         --force \
@@ -43,49 +48,52 @@ for ICAO in ${test_stations_string}; do
         --deps src/preprocessing.jl \
         --deps src/GPrealisations.jl \
         --deps src/predict_from_nearby.jl \
-        --deps data/outputs/fitted_kernel/mll/${GPmodel}/hyperparams_${GPmodel}_${ICAO}.json \
+        --deps ${kerneljson} \
         --outs data/outputs/predictions_from_nearby/mll/${GPmodel}/${ICAO} \
-        julia --project=batch batch/pipeline1.jl ${ICAO} ${GPmodel} ./data ./data/outputs \
+        julia --project=batch batch/pipeline1.jl ${ICAO} ${GPmodel} ./data ./data/outputs ${kerneljson} \
             --knearest=5 \
             & # do in parallel
 done
 
+wait
+
 ksmoothmax=50
 epsilon=0.01
 
-wait
-
+echo "--- compile Stan ---"
 compiledstandir=$(julia --project=batch batch/pipeline2.jl \
     outputdir \
     KBDL \
     SExSE \
     1 \
     ./data \
-    ./data/outputs/compiledstan \
-    --seed=42 \
-    --ksmoothmax=Inf \
-    --epsilon=Inf
+    ./data/outputs/compiledstan
 )
+echo "compiled stan directory: " $compiledstandir
 dvc run \
     --name compilestan \
     --force \
     --wdir $ROOT \
     --deps batch/pipeline2.jl \
     --deps src/stan_impute.jl \
-    --outs ${compiledstandir} \
+    --outs "${compiledstandir}/imputation.stan" \
+    --outs "${compiledstandir}/imputation.o" \
+    --outs "${compiledstandir}/imputation.hpp" \
+    --outs "${compiledstandir}/imputation.d" \
+    --outs "${compiledstandir}/imputation" \
     julia --project=batch batch/pipeline2.jl \
         compilestan \
         KBDL \
         SExSE \
         1 \
         ./data \
-        ./data/outputs/compiledstan \
-        --seed=42 \
-        --ksmoothmax=Inf \
-        --epsilon=Inf
+        ./data/outputs/compiledstan
 
 wait 
+
+echo "--- stan imputations ---"
 for ICAO in ${test_stations_string}; do
+    echo "ICAO stan imputations: " $ICAO
     for windownum in {1..3}; do
         # control the number of simultaneous processes
         outputdir=$(julia --project=batch batch/pipeline2.jl \
@@ -94,12 +102,9 @@ for ICAO in ${test_stations_string}; do
             $GPmodel \
             ${windownum} \
             ./data \
-            ./data/outputs \
-            --seed=${windownum} \
-            # --crossval \
-            --ksmoothmax=$ksmoothmax \
-            --epsilon=$epsilon)
-        cp ${compiledstandir}/* ${outputdir}/*
+            ./data/outputs
+            )
+        cp -R ${compiledstandir}/* ${outputdir}/
         (i=i%nbatch) ; ((i++==0)) && wait
         dvc run \
             --name imputations_${ICAO}_${GPmodel}_${windownum} \
@@ -110,19 +115,46 @@ for ICAO in ${test_stations_string}; do
             --deps src/preprocessing.jl \
             --deps src/stan_impute.jl \
             --deps data/outputs/predictions_from_nearby/mll/${GPmodel}/${ICAO} \
-            --deps $compiledstandir \
-            --outs ${outputdir} \
-            julia --project=batch batch/pipeline2.jl \
-                impute \
-                $ICAO \
-                $GPmodel \
-                ${windownum} \
-                ./data \
-                ./data/outputs \
-                --seed=${windownum} \
-                # --crossval \
-                --ksmoothmax=$ksmoothmax \
-                --epsilon=$epsilon &
+            --deps "${compiledstandir}/imputation.stan" \
+            --deps "${compiledstandir}/imputation.o" \
+            --deps "${compiledstandir}/imputation.hpp" \
+            --deps "${compiledstandir}/imputation.d" \
+            --deps "${compiledstandir}/imputation" \
+            --outs "${outputdir}/imputation.stan" \
+            --outs "${outputdir}/imputation.o" \
+            --outs "${outputdir}/imputation.hpp" \
+            --outs "${outputdir}/imputation.d" \
+            --outs "${outputdir}/imputation_1.data.R" \
+            --outs "${outputdir}/imputation_samples_1.csv" \
+            --outs "${outputdir}/imputation_samples_2.csv" \
+            --outs "${outputdir}/imputation_samples_3.csv" \
+            --outs "${outputdir}/imputation_samples_4.csv" \
+            "
+                julia --project=batch batch/pipeline2.jl \
+                    impute \
+                    $ICAO \
+                    $GPmodel \
+                    ${windownum} \
+                    ./data \
+                    ./data/outputs \
+                    --seed=${windownum} \
+                    --ksmoothmax=$ksmoothmax \
+                    --epsilon=$epsilon;
+                # remove last 4 lines of Stan output, which contains timings
+                # and is therefore not reproducible
+                grep -v ' seconds' \"${outputdir}/imputation_samples_1.csv\" > \"${outputdir}/tmp.csv\" \
+                    && mv \"${outputdir}/tmp.csv\" \"${outputdir}/imputation_samples_1.csv\";
+                grep -v ' seconds' \"${outputdir}/imputation_samples_2.csv\" > \"${outputdir}/tmp.csv\" \
+                    && mv \"${outputdir}/tmp.csv\" \"${outputdir}/imputation_samples_2.csv\";
+                grep -v ' seconds' \"${outputdir}/imputation_samples_3.csv\" > \"${outputdir}/tmp.csv\" \
+                    && mv \"${outputdir}/tmp.csv\" \"${outputdir}/imputation_samples_3.csv\";
+                grep -v ' seconds' \"${outputdir}/imputation_samples_4.csv\" > \"${outputdir}/tmp.csv\" \
+                    && mv \"${outputdir}/tmp.csv\" \"${outputdir}/imputation_samples_4.csv\";
+
+                rm \"${outputdir}/imputation_2.data.R\";
+                rm \"${outputdir}/imputation_3.data.R\";
+                rm \"${outputdir}/imputation_4.data.R\";
+            " &
     done
 done
 
